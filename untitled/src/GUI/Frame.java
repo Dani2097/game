6ee665ab5fc67fa2015e3f9f03a6cf5f39404349
dani2097 <danilo.sprovieri9@gmail.com>
2018-12-17 21:54:59 +0100

    first

--- untitled/src/GUI/Frame.java
+++ untitled/src/GUI/Frame.java
@@ -1,11 +1,14 @@
 package GUI;
 
+import Entities.Pokemon;
 import GUI.Logic.HotArea;
 
 import javax.swing.*;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.util.Scanner;
 
 public class Frame extends JFrame {
     public int width = 960, height = 960;
@@ -39,6 +42,40 @@ public class Frame extends JFrame {
             System.out.println("Errore: " + f);
             System.exit(1);
         }
+    }
+
+    public Pokemon getPokemonById(int id){
+        Pokemon p=new Pokemon();
+        Scanner file = null;
+        int k;
+        try{
+            file = new Scanner(new File("Pokedex.txt"));
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+
+            String line = file.nextLine();
+            p.id=Integer.parseInt(line.substring(3, 3+1));
+            line = file.nextLine();
+            k=Integer.parseInt(line.substring(11,12));
+            line = file.nextLine();
+            p.name=line.substring(5,5+k);
+        line = file.nextLine();
+        p.captureRate=Integer.parseInt(line.substring(12,14));
+        line = file.nextLine();
+        p.experienceGrowth=Integer.parseInt(line.substring(17,20));
+        line = file.nextLine();
+        p.type=Integer.parseInt(line.substring(5,6));
+        line = file.nextLine();
+        p.moveset=Integer.parseInt(line.substring(8,9));
+        line = file.nextLine();
+        p.sprite=Integer.parseInt(line.substring(7,8));
+        line = file.nextLine();
+        p.hp=Integer.parseInt(line.substring(10,12));
+
+        p.attack=Integer.parseInt(line.substring(13,15));
+
+        return p;
     }
 
 }
